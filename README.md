a1312f7e1e953d10056466e1c5d98e321b6a92ec
Alex <fastex93@gmail.com>
2016-06-26 14:58:13 +0500

    added README file

---
+++ README.md
@@ -0,0 +1,21 @@
+# Picbig
+
+Picbig is a simple lightbox library, represented as a react component.
+
+## Install
+
+```sh
+$ npm install picbig
+```
+
+## Usage
+
+```xml
+<Picbig small="/examples/images/image1_s.jpg" big="/examples/images/image1_b.jpg" />
+```
+
+With title mark:
+
+```xml
+<Picbig small="/examples/images/image1_s.jpg" big="/examples/images/image1_b.jpg" title="Star Wars" />
+```
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
-    './src/index'
+    './examples/index'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
@@ -20,7 +20,7 @@ module.exports = {
     loaders: [{
       test: /\.js$/,
       loaders: ['react-hot', 'babel'],
-      include: [path.join(__dirname, 'src'), path.join(__dirname, 'lib')]
+      include: [path.join(__dirname, 'examples'), path.join(__dirname, 'lib')]
     }]
   }
 };
