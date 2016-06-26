995d95a2e6281f66ccac9150538a912defda1ce8
Alex <fastex93@gmail.com>
2016-06-26 23:57:17 +0500

    fix images path

--- examples/App.js
+++ examples/App.js
@@ -30,22 +30,22 @@ export default class App extends Component {
 
 							<p className="lead">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
 
-							<Picbig className="pb1" small="/examples/images/image1_s.jpg" big="/examples/images/image1_b.jpg" title="Star Wars" />
+							<Picbig className="pb1" small="./examples/images/image1_s.jpg" big="./examples/images/image1_b.jpg" title="Star Wars" />
 
 							<p className="lead">The Big Oxmox advised her not to do so, because there were thousands of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t listen. She packed her seven versalia, put her initial into the belt and made herself on the way. When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
 
-							<Picbig className="pb2" small="/examples/images/image2_s.jpg" big="/examples/images/image2_b.jpg" />
+							<Picbig className="pb2" small="./examples/images/image2_s.jpg" big="./examples/images/image2_b.jpg" />
 
 							<p className="lead">A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now.</p>
 
 							<p className="lead">When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary, I throw myself down among the tall grass by the trickling stream; and, as I lie close to the earth, a thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects and flies, then I feel the presence of the Almighty, who formed us in his own image, and the breath.</p>
 
 							<div className="pb-container">
-								<Picbig className="pb3" group="gallery" small="/examples/images/image3_s.jpg" big="/examples/images/image3_b.jpg" />
+								<Picbig className="pb3" group="gallery" small="./examples/images/image3_s.jpg" big="./examples/images/image3_b.jpg" />
 
-								<Picbig className="pb5" group="gallery" small="/examples/images/image5_s.jpg" big="/examples/images/image5_b.jpg" title="Booker Dewitt" />
+								<Picbig className="pb5" group="gallery" small="./examples/images/image5_s.jpg" big="./examples/images/image5_b.jpg" title="Booker Dewitt" />
 
-								<Picbig className="pb4" group="gallery" small="/examples/images/image4_s.jpg" big="/examples/images/image4_b.jpg" title="Last of Us art" />
+								<Picbig className="pb4" group="gallery" small="./examples/images/image4_s.jpg" big="./examples/images/image4_b.jpg" title="Last of Us art" />
 							</div>
 
 							<br />
